64837ae6f2514b1a12c028578623154d23885314
Monica Tamaru Jones <mntamaru@gmail.com>
2019-11-13 01:46:51 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
 // Your code here
+function saturdayFun (activity="roller-skate") {
+  return `This Saturday, I want to ${activity}!`;
+}
+
+function mondayWork(activity="go to the office") {
+  return `This Monday, I will ${activity}.`;
+}
+
+function wrapAdjective(para="*") {
+  return function(adj="special") {
+    return `You are ${para}${adj}${para}!`
+  };
+}
+
+const Calculator = {
+  add : function(n1,n2) { 
+       return n1 + n2;
+  },
+    
+  subtract : function(n1, n2) {
+    return n1 -n2;
+  },
+  
+  multiply : function(n1,n2) {
+    return n1 * n2;
+  },
+  
+  divide: function(n1, n2) {
+    return n1/n2;
+  }
+};
+
+
+function actionApplyer(int, arr) {
+	let a = int;
+	
+	arr.forEach(meth => {
+		return a = meth(a);
+	});
+
+	return a;
+}
